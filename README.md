e22b91905caf627b69b869fd767f445477d998bf
Vadym Tyemirov <vadym.tyemirov@blackrock.com>
2018-10-10 08:59:52 -0700

    More examples added

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Using Kotlin with Apache Spark
 
-A simple example showing how to use Kotlin with Apache Spark. 
+A simple example showing how to use Kotlin with Apache Spark.
 
 For more information on the Kotlin language, visit the [Kotlin home page](http://kotlinlang.org/) or check out the [official O'Reilly course](https://player.oreilly.com/videos/9781491964118).
 
 Also read [my blog](http://tomstechnicalblog.blogspot.com/2016/10/kotlin-for-data-science.html) as I explore possible data science applications with Kotlin.
+
+## Running
+
+`gradle runExample`
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,28 +1,62 @@
+@file:JvmName("Example")
+
+package com.futureadvisor.algoio.kotlin.example
 
 import org.apache.spark.SparkConf
 import org.apache.spark.api.java.JavaSparkContext
 import kotlin.reflect.KClass
 
-fun main(args: Array<String>) {
+//extension function to register Kotlin classes
+fun SparkConf.registerKryoClasses(vararg args: KClass<*>) = registerKryoClasses(args.map { it.java }.toTypedArray())
 
-    class MyItem(val id: Int, val value: String)
+data class MyItem(val id: Int, val value: String)
 
-    val conf = SparkConf()
-            .setMaster("local")
-            .setAppName("Line Counter")
+fun kingLearExample(sc: JavaSparkContext): Long {
+    val home = System.getenv("HOME")
+    val kingLearPath = "$home/Documents/Development/src/algoIO/kinglear.txt"
 
-    conf.registerKryoClasses(MyItem::class)
+    val fileNameContentsRDD = sc.textFile(kingLearPath, 1).cache()
+    val totalWords = fileNameContentsRDD.flatMap { it.split(Regex("[\n\r\t ]")).iterator() }
 
-    val sc = JavaSparkContext(conf)
+    return totalWords.count()
+}
 
+fun sumNumbers(sc: JavaSparkContext): Int {
+    val items = listOf("123/643/7563/2134/ALPHA", "2343/6356/BETA/2342/12", "23423/656/343")
+
+    val input = sc.parallelize(items)
+
+    return input
+        .flatMap { it.split("/").iterator() }
+        .filter { it.matches(Regex("[0-9]+")) }
+        .map { it.toInt() }
+        .reduce { total, next -> total + next }
+}
+
+fun collectLetters(sc: JavaSparkContext): List<String> {
     val input = sc.parallelize(listOf(MyItem(1,"Alpha"), MyItem(2,"Beta")))
 
-    val letters = input.flatMap { it.value.split(Regex("(?<=.)")) }
+    val letters = input
+            .flatMap { it.value.split(Regex("(?<=.)")).iterator() }
             .map(String::toUpperCase)
             .filter { it.matches(Regex("[A-Z]")) }
 
-    println(letters.collect())
+    return letters.collect()
 }
 
-//extension function to register Kotlin classes
-fun SparkConf.registerKryoClasses(vararg args: KClass<*>) = registerKryoClasses(args.map { it.java }.toTypedArray())
+fun main(args: Array<String>) {
+    with(SparkConf()) {
+        val master = if(args.isNotEmpty()) args[0] else "local"
+
+        setMaster(master)
+        setAppName("Kotlin Spark Test")
+
+        registerKryoClasses(MyItem::class)
+
+        JavaSparkContext(this).let {
+            println("total: ${sumNumbers(it)}")
+            println("words: ${kingLearExample(it)}")
+            println("string: ${collectLetters(it)}")
+        }
+    }
+}
